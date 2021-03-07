dd6bb32f02d14bf0d5bfa0c143e24159172bfa08
MSRayed <77779340+MSRayed@users.noreply.github.com>
2021-03-07 15:02:15 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var sliders = {}; // All the sliders will be stored in here
 var canvas_sizes = {};
+var lines = [];
 
 var x = 0;
 var y = 0;
@@ -11,21 +12,34 @@ function setup() {
   createCanvas(500, 700);
 
   sliders = {
-    "spacing" : makeSlider(5, 50, 10, 5, initialize, "Spacing"), //min, max, start, step, callback, title_text
+    "spacing" : makeSlider(5, 50, 10, 2, initialize, "Spacing"), //min, max, start, step, callback, title_text
     "forward_sl_prob": makeSlider(0, 1, 0.5, 0.1, null, "Probability of the forward slash"),
     "x_limit_dec": makeSlider(-10, 10, 0, 1, null, "X limit decrement or Increment"),
+    "borderSize": makeSlider(0, 30, 10, 5, changeBorder, "Border Thickness"),
+
+    // Circle of doom
+    "codSize": makeSlider(0, 100, 50, 5, changeCOD, "COD Size"),
+    "codX": makeSlider(0, 100, 50, 1, changeCOD, "COD x"),
+    "codY": makeSlider(0, 100, 50, 1, changeCOD, "COD y"),
   }
 
   canvas_sizes = {
     "A3": [842, 1191],
-    "A4": [595, 842]
+    "A4": [595, 842],
+    "A5": [420, 595],
+    "A6": [298, 420],
   }
 
-  canvas_size = createSelect();
+  // Some other dom elements
+  canvas_size = createSelect(); // The selection of sizes object
   canvas_size.parent(select(".control-panel"))
-  canvas_size.option('A3');
-  canvas_size.option('A4');
+  
+  for (const property in canvas_sizes) {
+    canvas_size.option(property)
+  }
+
   canvas_size.changed(set_canvas_size);
+
   set_canvas_size();
 
   x_limit = width;
@@ -36,7 +50,25 @@ function setup() {
 
 
 function draw() {
+  borderSize = sliders['borderSize'].value();
+
+  push();
+  strokeWeight(borderSize);
+  stroke(200);
+  noFill();
+  rect(0, 0, width, height);
+  pop();
+
+  // Drawing the circle of doom
+  if (sliders['codSize'].value() != 0) {
+    codsize = sliders['codSize'].value();
+    codx = map(sliders['codX'].value(), 0, 100, 0, width);
+    cody = map(sliders['codY'].value(), 0, 100, 0, height);
+    ellipse(codx, cody, codsize, codsize);
+  }
+
   stroke(255);
+  
   if (random(1) < sliders["forward_sl_prob"].value()) {
     line(x, y, x+spacing, y+spacing);
   } else {
@@ -45,14 +77,14 @@ function draw() {
 
   x += spacing;
 
-  if (x > x_limit) {
+  if (x > x_limit-borderSize) {
     x = 0;
     y += spacing;
 
     x_limit += sliders['x_limit_dec'].value() * spacing;
-    console.log(sliders['x_limit_dec'].value());
   }
-  if (y > height) {
+
+  if (y > height-borderSize) {
     noLoop();
   }
 }
@@ -62,6 +94,17 @@ function set_canvas_size() {
   y = 0;
   resizeCanvas(canvas_sizes[canvas_size.value()][0], canvas_sizes[canvas_size.value()][1]);
   background(0);
+}
+
+function changeBorder() {
+  borderSize = sliders['borderSize'].value();
+  translate(borderSize, borderSize);
+}
+
+function changeCOD() {
+  background(0);
+  x = 0;
+  y = 0;
 }
 
 function makeSlider(min, max, start, step, callback=null, title_text) { // To create the slider adding an input event to it
